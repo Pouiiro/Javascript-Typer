b3a91400b34aba542419543d0ab209c2bd75ceda
ouailb <ouailbni@gmail.com>
2022-02-17 10:53:22 +0100

    feat: iteration 2

--- frontend/src/Provider/index.jsx
+++ frontend/src/Provider/index.jsx
@@ -1,10 +1,13 @@
 import React, { createContext } from 'react'
 import PropTypes from 'prop-types'
+import StaticQuotes from 'Data/quotes.json'
 
 export const AppContext = createContext()
 
 const ProviderV = ({ children }) => {
-  const providerValue = {}
+  let quoties = StaticQuotes
+  let favs = []
+  const providerValue = { quoties, favs }
   return (
     <AppContext.Provider value={providerValue}>{children}</AppContext.Provider>
   )
